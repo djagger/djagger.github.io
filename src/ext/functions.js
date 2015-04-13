4e0ea5e95b2c26333ac2c29663fba31cf7a1666c
djagger <oleg.mysin@yahoo.com>
2015-04-13 12:52:28 +0300

    update

--- src/ext/functions.js
+++
@@ -1,8 +0,0 @@
-Function.prototype.curry = function () {
-	var slice = Array.prototype.slice;
-	var fn = this;
-	var args = slice.call(arguments);
-	return function () {
-		fn.apply(this, args.concat(slice.call(arguments)));
-	}
-};
